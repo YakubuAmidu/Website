f0feb1a41bb8f47008ef4317dc4a02d65ad58708
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-15 05:01:10 -0800

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './components/App.css';
-import { Router, Switch, Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import App from "./components/App";
-import Portfolio from "./components/Portfolio";
-import Jokes from "./components/Jokes";
-import MusicMaster from "./projects/music-master";
-import SocialProfiles from "./components/SocialProfiles";
-import Reactone from "./screens/Reactone";
-import Reacttwo from "./screens/Reacttwo";
-import Reactthree from "./screens/Reactthree";
-import Reactfour from "./screens/Reactfour";
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import App from './components/App';
+import Portfolio from './components/Portfolio';
+import Jokes from './components/Jokes';
+import MusicMaster from './projects/music-master';
+import SocialProfiles from './components/SocialProfiles';
+import Reactone from './screens/Reactone';
+import Reacttwo from './screens/Reacttwo';
+import Reactthree from './screens/Reactthree';
+import Reactfour from './screens/Reactfour';
 
 ReactDOM.render(
   <Router history={createBrowserHistory()}>
-  <Switch>
-  <Route exact path="/" component={App}/>
-  <Route path="/portfolio" component={Portfolio} />
-  <Route path="/jokes" component={Jokes} />
-  <Route path="/music-master" components={MusicMaster} />
-  <Route path="/socialProfiles" component={SocialProfiles} />
- <Route path="/reactone" components={Reactone} />
- <Route path="/reacttwo" components={Reacttwo} />
- <Route path="/reactthree" components={Reactthree} />
- <Route path="/reactfour" components={Reactfour} />
-  </Switch>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/portfolio' component={Portfolio} />
+      <Route path='/jokes' component={Jokes} />
+      <Route path='/music-master' components={MusicMaster} />
+      <Route path='/socialProfiles' component={SocialProfiles} />
+    </Switch>
   </Router>,
   document.getElementById('root')
 );
