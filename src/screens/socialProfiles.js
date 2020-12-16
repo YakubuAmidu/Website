5c93887267ef990aa11c6a93a75fbfea26906ef2
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-15 22:49:39 -0800

    Initial commit

--- src/screens/socialProfiles.js
+++ src/screens/socialProfiles.js
@@ -1,39 +1,39 @@
-import emailIcon from "../assets/email.png";
-import linkedinIcon from "../assets/linkedin.png";
-import facebookIcon from "../assets/facebook.png";
-import githubIcon from "../assets/github.png";
-import instagramIcon from "../assets/instagram.png";
-import twitterIcon from "../assets/twitter.png";
+import emailIcon from '../assets/email.png';
+import linkedinIcon from '../assets/linkedin.png';
+import facebookIcon from '../assets/facebook.png';
+import githubIcon from '../assets/github.png';
+import instagramIcon from '../assets/instagram.png';
+import twitterIcon from '../assets/twitter.png';
 
 const SOCIAL_PROFILES = [
   {
     id: 1,
-    link: "mailto: yakubu.amidu@yahoo.com",
+    link: 'mailto: yakubu.amidu@yahoo.com',
     image: emailIcon,
   },
   {
     id: 2,
-    link: "https://www.linkedin.com/in/yakubu-amidu/",
+    link: 'https://www.linkedin.com/in/yakubu-amidu/',
     image: linkedinIcon,
   },
   {
     id: 3,
-    link: "https://www.facebook.com/yakubu.amidu.3304",
+    link: 'https://www.facebook.com/yakubu.amidu.3304',
     image: facebookIcon,
   },
   {
     id: 4,
-    link: "https://github.com/YakubuAmidu",
+    link: 'https://github.com/YakubuAmidu',
     image: githubIcon,
   },
   {
     id: 5,
-    link: "https://www.instagram.com/yakubuamidu19/",
+    link: 'https://www.instagram.com/yakubuamidu19/',
     image: instagramIcon,
   },
   {
     id: 6,
-    link: "https://www.linkedin.com/in/yakubu-amidu/",
+    link: 'https://twitter.com/yakubuamidu19',
     image: twitterIcon,
   },
 ];
